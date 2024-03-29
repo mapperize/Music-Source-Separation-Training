90aaae37c1d26075bb78a8c1eb8d22ccb2105a6e
Joowon <joowonpark400@gmail.com>
2024-03-29 14:33:11 -0400

    2

--- models/bs_mamba/bs_mamba.py
+++ models/bs_mamba/bs_mamba.py
@@ -71,7 +71,7 @@ class MoELayer(nn.Module):
 
         self.experts = nn.ModuleList(
             [
-                Mamba(d_model=d_models, d_state=d_state, d_conv=d_conv, expand=expand, eps=eps)
+                Mamba(d_model=d_model, d_state=d_state, d_conv=d_conv, expand=expand, eps=eps)
                 for i in range(num_experts)
             ]   
         )
