40af714c2efb46cf5ddfc3210ff16a3e8668717f
Joowon <joowonpark400@gmail.com>
2024-03-29 14:36:29 -0400

    2

--- models/bs_mamba/bs_mamba.py
+++ models/bs_mamba/bs_mamba.py
@@ -93,10 +93,10 @@ class MoELayer(nn.Module):
             for k in range(self.top_k):
                 indices = (k_indices[:, k] == idx).nonzero()
                 if indices.numel() > 0:
-                    x[indices] = expert(x_view[indices], inference_params = params)
-                    x[indices] *= k_probs[:, k][indices].unsqueeze(1)
+                    x_view[indices] = expert(x_view[indices], inference_params = params)
+                    x_view[indices] *= k_probs[:, k][indices].unsqueeze(1)
 
-        x = x.view(*x_shape)
+        x = x_view.view(*x_shape)
         return x, residual
 
 class MambaLayer(nn.Module):
