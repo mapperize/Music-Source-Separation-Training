a866351dbdbb5b5056b85e110429886a8d79819e
Joowon <joowonpark400@gmail.com>
2024-03-28 22:15:01 -0400

    idk why im using github instead of just editting directly

--- models/bs_mamba/bs_mamba.py
+++ models/bs_mamba/bs_mamba.py
@@ -148,14 +148,13 @@ class MambaModule(nn.Module):
             'd_state': attn_state, 'd_Conv': attn_conv, 'expand': attn_expand
         }
         # I have no clue when putting multiple classes in a list inside ModuleList causes error
-        self.layers = nn.ModuleList([
-            nn.Sequential([
-            MambaLayer(d_model=d_model, layer_idx=i, eps=eps, **kwargs_attn),
-            layer(d_model=d_model, layer_idx=i, eps=eps, **kwargs_ff)
-            ])
+        self.layers = [
+            (
+                MambaLayer(d_model=d_model, layer_idx=i, eps=eps, **kwargs_attn),
+                layer(d_model=d_model, layer_idx=i, eps=eps, **kwargs_ff)
+            )
             for i in range(depth)
-        ])
- 
+        ]
 
         self.norm = fusedRMSNorm(d_model, eps = eps)
 
