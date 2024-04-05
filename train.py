dad36dbebc8070460c4a1c24281c56d6f0c2aa56
mapperize <120153524+mapperize@users.noreply.github.com>
2024-04-05 16:19:32 -0400

    Update train.py
    forgot to add bs_mamba

--- train.py
+++ train.py
@@ -450,7 +450,7 @@ def train_model(args):
             x = mixes.to(device)  # mixture
 
             with torch.cuda.amp.autocast(enabled=use_amp):
-                if args.model_type in ['mel_band_roformer', 'bs_roformer']:
+                if args.model_type in ['bs_mamba', 'mel_band_roformer', 'bs_roformer']:
                     # loss is computed in forward pass
                     loss = model(x, y)
                     if type(device_ids) != int:
